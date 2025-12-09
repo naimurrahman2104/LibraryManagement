bc7e4341097362b3684cc9b10d30d39bda8f835e
Rahman Md Naimur <naimurrahman2104@gmail.com>
2025-12-09 20:53:25 +0900

    Update Book.cpp
    added comment

--- src/Book.cpp
+++ src/Book.cpp
@@ -1,8 +1,9 @@
 #include "Book.hpp"
 
-Book::Book(int id, string title, string author, int count)
+Book::Book(int id, string title, string author, int count) // book constructor
     : Item(id, title, count), author(author) {}
 
+// show book info
 void Book::display() const {
     cout << "[Book] ID: " << id
          << " | Title: " << title
@@ -11,6 +12,7 @@ void Book::display() const {
          << "\n";
 }
 
+// convert book to text
 string Book::serialize() const {
     return "BOOK," + to_string(id) + "," + title + "," + author + "," +
            to_string(availableCount);
